b4548d30ec8246bc361dc93127c64a14196dcf08
Eno Hoxha <enooohoxha@gmail.com>
2023-10-03 23:38:45 +0200

    changes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span> </span>
-        <Image src="." alt="" />
+       
         
         <p className="text-slate-600">All rights reserved  @ENO.</p>
       </div>
