c1bdeac3af79866ba61c3050720e5fb05a1305a8
Eno Hoxha <enooohoxha@gmail.com>
2023-10-03 23:14:45 +0200

    fixings

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span> </span>
-        <img src="." alt="" />
+        <Image src="." alt="" />
         
         <p className="text-slate-600">All rights reserved  @ENO.</p>
       </div>
