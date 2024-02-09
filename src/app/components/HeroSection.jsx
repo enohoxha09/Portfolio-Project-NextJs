45584ddabaab397cdb420bc34f515ea2f2e19a18
Eno Hoxha <enooohoxha@gmail.com>
2024-02-09 10:33:41 +0100

    update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,7 +45,6 @@ const HeroSection = () => {
               href="#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
             </Link>
             <Link
               href="https://www.mediafire.com/file/30ckmvzprx4ls4d/EnO.pdf/file"
